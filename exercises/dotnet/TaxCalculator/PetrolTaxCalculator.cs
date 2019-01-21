980b6af66c9ed34f9b06c6b61bc30d450b2704ab
Peter Askey <Peter_Askey@waters.com>
2019-01-21 14:01:45 +0000

    [PA COMMIT STORY 3] Add alternative fuel functionality calculator

--- exercises/dotnet/TaxCalculator/PetrolTaxCalculator.cs
+++ exercises/dotnet/TaxCalculator/PetrolTaxCalculator.cs
@@ -8,34 +8,123 @@ namespace TaxCalculator
     {
         public override int CalculateTax(Vehicle vehicle)
         {
+            
             int emissions = vehicle.Co2Emissions;
 
-            if (emissions < 1)
-                return 0;
-            else if (emissions < 51)
-                return 10;
-            else if (emissions < 76)
-                return 25;
-            else if (emissions < 91)
-                return 105;
-            else if (emissions < 101)
-                return 125;
-            else if (emissions < 111)
-                return 145;
-            else if (emissions < 131)
-                return 165;
-            else if (emissions < 151)
-                return 205;
-            else if (emissions < 171)
-                return 515;
-            else if (emissions < 191)
-                return 830;
-            else if (emissions < 226)
-                return 1240;
-            else if (emissions < 256)
-                return 1760;
+            if (vehicle.FuelType == FuelType.Petrol)
+            {
+                if (emissions < 1)
+                {
+                    return 0;
+                }
+                else if (emissions < 51)
+                {
+                    return 10;
+                }
+                else if (emissions < 76)
+                {
+                    return 25;
+                }
+                else if (emissions < 91)
+                {
+                    return 105;
+                }
+                else if (emissions < 101)
+                {
+                    return 125;
+                }
+                else if (emissions < 111)
+                {
+                    return 145;
+                }
+                else if (emissions < 131)
+                {
+                    return 165;
+                }
+                else if (emissions < 151)
+                {
+                    return 205;
+                }
+                else if (emissions < 171)
+                {
+                    return 515;
+                }
+                else if (emissions < 191)
+                {
+                    return 830;
+                }
+                else if (emissions < 226)
+                {
+                    return 1240;
+                }
+                else if (emissions < 256)
+                {
+                    return 1760;
+                }
+                else
+                {
+                    return 2070;
+                }
+            }
+            else if (vehicle.FuelType == FuelType.AlternativeFuel)
+            {
+                if (emissions < 1)
+                {
+                    return 0;
+                }
+                else if (emissions < 51)
+                {
+                    return 0;
+                }
+                else if (emissions < 76)
+                {
+                    return 15;
+                }
+                else if (emissions < 91)
+                {
+                    return 95;
+                }
+                else if (emissions < 101)
+                {
+                    return 115;
+                }
+                else if (emissions < 111)
+                {
+                    return 135;
+                }
+                else if (emissions < 131)
+                {
+                    return 155;
+                }
+                else if (emissions < 151)
+                {
+                    return 195;
+                }
+                else if (emissions < 171)
+                {
+                    return 505;
+                }
+                else if (emissions < 191)
+                {
+                    return 820;
+                }
+                else if (emissions < 226)
+                {
+                    return 1230;
+                }
+                else if (emissions < 256)
+                {
+                    return 1750;
+                }
+                else
+                {
+                    return 2060;
+                }
+            }
             else
-                return 2070;
+            {
+                return 0;
+            }
         }
     }
 }
